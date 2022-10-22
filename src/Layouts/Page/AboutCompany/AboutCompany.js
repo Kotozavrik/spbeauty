236eef06b5ae0f438ecb52e0cca87c96963878f6
Victoria <akhmetova.littlecthulhu@yandex.ru>
2022-10-22 22:41:18 +0000

    Update 11 files

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -2,15 +2,23 @@ import Heading from "./Components/Heading";
 import General from "./Components/General/General";
 import Addresses from "./Components/Addresses/Addresses";
 function AboutCompany() {
-    return (
+  return (
+    <div className="ms-72px me-72px mt-64px">
+      <Heading /> {/* Заголовок и кнопка справа */}
+      <div className="d-flex pt-5">
+        <div className="flex-grow-1">
+          {" "}
+          <General />{" "}
+        </div>{" "}
+        {/* Основная информация вроде названия, типа и телефона */}
         <div>
-            <Heading/> {/* Заголовок и кнопка справа */}
-            <div className='d-flex flex-column mt-5'>
-                <General/>  {/* Основная информация вроде названия, типа и телефона */}
-                <Addresses/> {/* Список доступных адресов заведений */}
-            </div>
-        </div>
-    );
+          {" "}
+          <Addresses />{" "}
+        </div>{" "}
+        {/* Список доступных адресов заведений */}
+      </div>
+    </div>
+  );
 }
 
 export default AboutCompany;
--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,17 +1,18 @@
-import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
-    return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
-            </div>
-        </div>
-    );
+  return (
+    <div className="d-inline-flex flex-column col gap-4 p-4 h-100 border border-1 rounded">
+      <div className="title ">Адреса заведений</div>
+      <div className="d-inline-flex flex-column gap-4">
+        <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+        <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+      </div>
+      <div className="d-inline-flex mt-auto">
+        <SecondaryButton>Добавить филиал</SecondaryButton>
+      </div>
+    </div>
+  );
 }
 
 export default Addresses;
